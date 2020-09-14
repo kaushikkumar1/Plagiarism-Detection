31f94b3812c4b042f852e81ac5e3189afb0ffb76
beingzero <beingzeroin@gmail.com>
2020-09-14 23:12:01 +0530

    Added Crawler Code for HackerRank Contest

---
+++ backend/lib/requestLib.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const log4js = require('log4js');
+const log = log4js.getLogger('requestLib');
+var config =require('../config/config').get_active_config();
+log.level = config.log4jsLevel;
+
+module.exports.makeAPIRequest = function(url, headers, callback){
+    request(url, { 'headers': headers}, function (err, response, body) {
+        if(err){
+            log.fatal(err);
+            callback(err, null);
+        }
+        else{
+            log.debug(JSON.stringify(headers));
+            log.debug('%s RESPONSE CODE %d', url, response.statusCode);
+            var jsonResult = JSON.parse(body);
+            log.debug('JSON: %s', JSON.stringify(jsonResult));
+            callback(err, jsonResult);
+        }
+    });
+}
