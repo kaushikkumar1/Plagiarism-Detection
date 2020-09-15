0fdd828e1408431c2594c0984077a75051a32b80
beingzero <beingzeroin@gmail.com>
2020-09-15 07:36:09 +0530

    Added only necessary cookie value

--- backend/config/scraperConfig.js
+++ backend/config/scraperConfig.js
@@ -3,7 +3,7 @@ module.exports = {
         agent : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
     },
     "hackerrank" : {
-        cookie : "_ga=XXXXXXXX; __utmb=74197771.7.10.1597707552",
+        cookie : "_hrank_session= XXXXXXXX;",
         hr_submission_id_to_code_url : "https://www.hackerrank.com/rest/contests/${contest_name}/submissions/${site_submission_id}",
         hr_judge_submissions_url : "https://www.hackerrank.com/rest/contests/${contest_name}/judge_submissions?offset=${offset}&limit=${limit}",
         hr_leaderboard_crawling_min_delay_milliseconds: 5000,
