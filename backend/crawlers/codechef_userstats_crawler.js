08c8d96ec05677b5ed8f971ea84d72558fb9bc2f
beingzero <beingzeroin@gmail.com>
2020-09-19 02:11:03 +0530

    Codechef Crawler Simplified

--- backend/crawlers/codechef_userstats_crawler.js
+++ backend/crawlers/codechef_userstats_crawler.js
@@ -1,71 +1,20 @@
-const scraperConfig = require('../config/scraperConfig');
 const template = require('lodash.template');
-const utilityLib = require('../lib/utilityLib');
-const userStasCrawlerLib = require('../lib/userStasCrawlerLib');
-const dbconnect = require('../db/connect');
-const itemLib = require('../lib/itemLib');
-const codingProfilesModel = require('../models/codingProfilesModel');
+const userStatsCrawler = require('./userstats_crawler');
+const scraperConfig = require('../config/scraperConfig');
 const codechefParser = require('../lib/parsers/codechefParser');
-
-const async = require('async');
-const moment = require('moment');
-
-
-var site_name = 'CODECHEF';
-
-var codechefSiteOptions = {
-    site_name : site_name,
-    site_parser : codechefParser,
-    userProfilePageUrl: template(scraperConfig[site_name].user_profile_url),
-    site_headers : {'User-Agent': scraperConfig.common.agent}
-};
-
-var siteOptions = codechefSiteOptions;
+const dbconnect = require('../db/connect');
 
 var connect_to_db = true;
 
 if(connect_to_db)
     dbconnect.connect(false);
 
+var codechefSiteOptions = {
+    site_name : 'CODECHEF',
+    site_parser : codechefParser,
+    userProfilePageUrl: template(scraperConfig['CODECHEF'].user_profile_url),
+    site_headers : {'User-Agent': scraperConfig.common.agent},
+    connect_to_db: false
+};
 
-
-function crawlLoop(n){
-    var allPages = [];
-    for (var page = 1; page <= n; page++)
-        allPages.push(page);
-    async.eachSeries(allPages, function(currentPage, next){
-        userStasCrawlerLib.pickNextUserCrawlAndUpdateDB(siteOptions, function(){
-            var timeoutDurationInMs = utilityLib.randInRange(scraperConfig.common.min_crawling_delay, scraperConfig.common.max_crawling_delay);
-            console.log('Timeout for '+timeoutDurationInMs + " ms")
-            setTimeout(() => {  next();  }, timeoutDurationInMs);
-        })
-    }, function(err){
-        console.log("COMPLETED CRAWLING");
-        if(connect_to_db)
-            dbconnect.disconnect();
-    });
-}
-
-// UNIT TEST
-//crawlLoop(1);
-
-function getCountToCrawl(cb){
-    var backinMins = moment(new Date()).subtract(scraperConfig.common.maximum_fetch_window_in_minutes, 'm').toDate();
-    var query = {site_name: site_name , is_handle_valid: {$ne: false}, is_crawler_active: {$ne: false}};
-    query['$or'] = [{last_crawled_at: {$lt: backinMins}}, {last_crawled_at: null}];
-    itemLib.getItemByQuery(query, codingProfilesModel, function(err, res){
-        if(err)
-            cb(0);
-        else
-            cb(res.length);
-    })
-}
-
-function dailyCrawlRun(){
-    getCountToCrawl(function(cnt){
-        console.log("GOT COUNT AS  "+cnt);
-        crawlLoop(cnt);
-    })
-}
-
-dailyCrawlRun();
+userStatsCrawler.dailyCrawl(codechefSiteOptions);
