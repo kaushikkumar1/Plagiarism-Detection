d0f344127bef799f4b0f0414cea0fbd5136b817c
beingzero <beingzeroin@gmail.com>
2020-09-25 13:45:35 +0530

    Updateing batch profiles table name and field name

---
+++ backend/models/batchProfilesModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var BatchSchemaProfiles = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    user_roll_number: {
+        type: String,
+        required: true
+    },
+    batch_name: {
+        type: String,
+        required: true
+    },
+});
+
+
+BatchSchemaProfiles.index({
+    user_roll_number: 1,
+    user_batch_name: 1
+}, {
+    unique: true
+});
+
+
+module.exports = mongoose.model("BatchProfiles", BatchSchemaProfiles);
